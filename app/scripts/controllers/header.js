d8cae88da8dcda7d1625d67c2461245e386e3704
jigkoh3 <jigkoh3@hotmail.com>
2015-10-28 07:45:22 +0700

    update recommend

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -10,8 +10,8 @@
 angular.module('fec3App')
   .controller('headerCtrl', function ($scope, $localstorage) {
   	$scope.isAuthen = false;
-  	var userProfile = $localstorage.getObject("userProfile");
-  	if(userProfile != null)
+  	var customerProfile = $localstorage.getObject("customerProfile");
+  	if(customerProfile != null)
   	{
   		$scope.isAuthen = true;
   	}
