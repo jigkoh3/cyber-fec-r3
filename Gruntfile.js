e7b02ed706d537faeb46eb2e927733f99ef72a9a
chang2535 <changoohlala@gmail.com>
2015-11-02 23:37:10 +0700

    delete_

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,12 +72,11 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-<<<<<<< HEAD
-        hostname: 'localhost'
-=======
+
+
         hostname: 'localhost'//,
         //livereload: 35729
->>>>>>> 8cb55229d0a7db8e3dbcf94ac79c1b97117e94c8
+
       },
       livereload: {
         options: {
