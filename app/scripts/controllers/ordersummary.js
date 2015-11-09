0ee7a5065e9b6f54045721ff51b71ba9a8784b61
jigkoh3 <jigkoh3@hotmail.com>
2015-11-09 12:11:41 +0700

    fix issue local satorage of oder item deslete

--- app/scripts/controllers/ordersummary.js
+++ app/scripts/controllers/ordersummary.js
@@ -9,132 +9,14 @@ angular.module('fec3App')
     var customerInfo = $localstorage.getObject("customerProfile");
     $scope.order_product_item_list = customerInfo.orderObj.order_product_item_list;
 
-    // $scope.order_product_item_list = [{
-    //        	"ORDER_ID": "1",
-    // 	"SEQUENCE": 1,
-    // 	"CAMPAIGN": "string",
-    // 	"CAMPAIGN_NAME": "CAMPAIGN 2",
-    // 	"PROMOTION_SET": "string",
-    // 	"PROMOTION_TYPE": "string",
-    // 	"CAMPAIGN_PROMO_ITEM_QTY": 2,
-    // 	"GROUP_ID": "222",
-    // 	"PRODUCT_TYPE": "string",
-    // 	"PRODUCT_CODE": "string",
-    // 	"PRODUCT_NAME": "3000036063 iPhone 6 Plus, 16GB, Gold",
-    // 	"PRICEPLAN_CODE": "string",
-    // 	"PRICEPLAN_NAME": "string",
-    // 	"SERVICE_REGISTER_TYPE": "string",
-    // 	"MOBILE_NUMBER": "string",
-    // 	"PRICE": 29450.00 ,
-    // 	"QTY": 1,
-    // 	"TOTAL": 1,
-    // 	"DISCOUNT_AMOUNT": 1.5,
-    // 	"DEPOSIT_AMOUNT": 1.5,
-    // 	"NET_AMOUNT": 29450,
-    // 	"OTHER_PAYMENT_AMOUNT": 1.5
-    // },{
-    // 	"ORDER_ID": "2",
-    // 	"SEQUENCE": 2,
-    // 	"CAMPAIGN": "string",
-    // 	"CAMPAIGN_NAME": "CAMPAIGN 1",
-    // 	"PROMOTION_SET": "string",
-    // 	"PROMOTION_TYPE": "string",
-    // 	"CAMPAIGN_PROMO_ITEM_QTY": 2,
-    // 	"GROUP_ID": "111",
-    // 	"PRODUCT_TYPE": "string",
-    // 	"PRODUCT_CODE": "string",
-    // 	"PRODUCT_NAME": "3000014920 Sim Card",
-    // 	"PRICEPLAN_CODE": "string",
-    // 	"PRICEPLAN_NAME": "string",
-    // 	"SERVICE_REGISTER_TYPE": "string",
-    // 	"MOBILE_NUMBER": "string",
-    // 	"PRICE": 69.00,
-    // 	"QTY": 1,
-    // 	"TOTAL": 1,
-    // 	"DISCOUNT_AMOUNT": 1.5,
-    // 	"DEPOSIT_AMOUNT": 1.5,
-    // 	"NET_AMOUNT": 69,
-    // 	"OTHER_PAYMENT_AMOUNT": 1.5
-    // },{
-    // 	"ORDER_ID": "3",
-    // 	"SEQUENCE": 3,
-    // 	"CAMPAIGN": "string",
-    // 	"CAMPAIGN_NAME": "CAMPAIGN 3",
-    // 	"PROMOTION_SET": "string",
-    // 	"PROMOTION_TYPE": "string",
-    // 	"CAMPAIGN_PROMO_ITEM_QTY": 1,
-    // 	"GROUP_ID": "333",
-    // 	"PRODUCT_TYPE": "string",
-    // 	"PRODUCT_CODE": "string",
-    // 	"PRODUCT_NAME": "3000014333 test",
-    // 	"PRICEPLAN_CODE": "string",
-    // 	"PRICEPLAN_NAME": "string",
-    // 	"SERVICE_REGISTER_TYPE": "string",
-    // 	"MOBILE_NUMBER": "string",
-    // 	"PRICE": 169.00,
-    // 	"QTY": 1,
-    // 	"TOTAL": 1,
-    // 	"DISCOUNT_AMOUNT": 1.5,
-    // 	"DEPOSIT_AMOUNT": 1.5,
-    // 	"NET_AMOUNT": 169,
-    // 	"OTHER_PAYMENT_AMOUNT": 1.5
-    // },{
-    // 	"ORDER_ID": "4",
-    // 	"SEQUENCE": 4,
-    // 	"CAMPAIGN": "string",
-    // 	"CAMPAIGN_NAME": "CAMPAIGN 1",
-    // 	"PROMOTION_SET": "string",
-    // 	"PROMOTION_TYPE": "string",
-    // 	"CAMPAIGN_PROMO_ITEM_QTY": 2,
-    // 	"GROUP_ID": "111",
-    // 	"PRODUCT_TYPE": "string",
-    // 	"PRODUCT_CODE": "string",
-    // 	"PRODUCT_NAME": "2200005555 test2",
-    // 	"PRICEPLAN_CODE": "string",
-    // 	"PRICEPLAN_NAME": "string",
-    // 	"SERVICE_REGISTER_TYPE": "string",
-    // 	"MOBILE_NUMBER": "string",
-    // 	"PRICE": 569.00,
-    // 	"QTY": 1,
-    // 	"TOTAL": 1,
-    // 	"DISCOUNT_AMOUNT": 1.5,
-    // 	"DEPOSIT_AMOUNT": 1.5,
-    // 	"NET_AMOUNT": 569,
-    // 	"OTHER_PAYMENT_AMOUNT": 1.5
-    // },{
-    // 	"ORDER_ID": "5",
-    // 	"SEQUENCE": 5,
-    // 	"CAMPAIGN": "string",
-    // 	"CAMPAIGN_NAME": "CAMPAIGN 2",
-    // 	"PROMOTION_SET": "string",
-    // 	"PROMOTION_TYPE": "string",
-    // 	"CAMPAIGN_PROMO_ITEM_QTY": 2,
-    // 	"GROUP_ID": "222",
-    // 	"PRODUCT_TYPE": "string",
-    // 	"PRODUCT_CODE": "string",
-    // 	"PRODUCT_NAME": "3344556677 test3",
-    // 	"PRICEPLAN_CODE": "string",
-    // 	"PRICEPLAN_NAME": "string",
-    // 	"SERVICE_REGISTER_TYPE": "string",
-    // 	"MOBILE_NUMBER": "string",
-    // 	"PRICE": -5000.00,
-    // 	"QTY": 1,
-    // 	"TOTAL": 1,
-    // 	"DISCOUNT_AMOUNT": 1.5,
-    // 	"DEPOSIT_AMOUNT": 1.5,
-    // 	"NET_AMOUNT": -5000,
-    // 	"OTHER_PAYMENT_AMOUNT": 1.5
-    // }];
-
-
-
+    
     $scope.order_add = [];
     $scope.removeRow = function(GROUP_ID) {
         var count = 0;
         //var indexlist = [];	
         var indexlist = -1;
         var oldgroup;
-        var listArr = eval($scope.order_product_item_list);
+        var listArr = $scope.order_product_item_list;
         for (var i = 0; i < listArr.length; i++) {
             if (listArr[i].GROUP_ID === GROUP_ID) {
                 $scope.order_product_item_list.splice(i, 1);
@@ -145,6 +27,9 @@ angular.module('fec3App')
                 $scope.totalCalculate();
             }
         }
+        customerInfo.orderObj.order_product_item_list = $scope.order_product_item_list;
+        $localstorage.setObject("customerProfile", customerInfo);
+        //$localstorage.logObject("customerProfile");
     };
 
     $scope.totalCalculate = function() {
