742d0ad89b5c49a381611ffea5ec2f96380e2c9a
tingtang35 <tingtang35@gmail.com>
2015-11-11 12:14:02 +0700

    update cart icon

--- app/scripts/controllers/pricePlan.js
+++ app/scripts/controllers/pricePlan.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('fec3App')
-    .controller('pricePlanCtrl', function($scope, productService) {
+    .controller('pricePlanCtrl', function($scope, $location, productService) {
         
         $scope.pricePlans = [{
             pricePlan: "BBSMEP26:Biz_BB BIS 699 3WEG Unlimited",
@@ -24,7 +24,7 @@ angular.module('fec3App')
             promotion: ""
         }];
 
-        $scope.detailPhonenumber = [{  
+        $scope.detailPhonenumber = [{
             phonenumber: "0891112222",
             simcard: "1",
             priceplan: "RMABCDO1:Real Move Very Good Price Plan",
@@ -34,67 +34,66 @@ angular.module('fec3App')
         $scope.openservice_obj = {
             status: 'new_number',
             customer_subtype: [{
-                    textname:'RPI: บุคคลธรรมดา',
-                    textvalue:'RPI'
-                },{
-                    textname:'RPO: บริษัท',
-                    textvalue:'RPO'
-                }],
-             
+                textname: 'RPI: บุคคลธรรมดา',
+                textvalue: 'RPI'
+            }, {
+                textname: 'RPO: บริษัท',
+                textvalue: 'RPO'
+            }],
             postPromotion: [{
-                    textname:'100932 : New Post Privilge Free HS',
-                    textvalue:'promotion_1'
-                },{
-                    textname:'100933 : New Post Privilge Free HS 2',
-                    textvalue:'promotion_2'
-                },{
-                    textname:'100934 : New Post Privilge Free HS 3',
-                    textvalue:'promotion_3'
-                }
-           ],
-            phonenumberReservations: [
-                {phone :'0865630240'},
-                {phone :'0891112222'},
-                {phone :'0863334444'}
-            ]
-
+                textname: '100932 : New Post Privilge Free HS',
+                textvalue: 'promotion_1'
+            }, {
+                textname: '100933 : New Post Privilge Free HS 2',
+                textvalue: 'promotion_2'
+            }, {
+                textname: '100934 : New Post Privilge Free HS 3',
+                textvalue: 'promotion_3'
+            }],
+            phonenumberReservations: [{
+                phone: '0865630240'
+            }, {
+                phone: '0891112222'
+            }, {
+                phone: '0863334444'
+            }]
         };
 
-    $scope.getPrivilegeSaleType = [{
-        "status": "SUCCESSFUL",
-        "display-messages": [],
-        "trx-id": "461HQMYTGV3HF",
-        "process-instance": "psaapdv1 (instance: SFF_node1)",
-        "response-data": {
-            "sale-type" : [ 
-                "NEW"
-            ],
-            "is-change-priceplan" : "true"
+        $scope.getPrivilegeSaleType = [{
+            "status": "SUCCESSFUL",
+            "display-messages": [],
+            "trx-id": "461HQMYTGV3HF",
+            "process-instance": "psaapdv1 (instance: SFF_node1)",
+            "response-data": {
+                "sale-type": [
+                    "NEW"
+                ],
+                "is-change-priceplan": "true"
+            }
+        }];
+
+
+        //var customersubtype_val = 'RPI';
+        //$scope.customer_subtype.val() = customersubtype_val;
+        $scope.customer_subtype = $scope.openservice_obj.customer_subtype[1].textvalue;
+        $scope.promotion = $scope.openservice_obj.postPromotion[1].textvalue;
+
+        $scope.addGroup = function() {
+            $scope.detailPhonenumber.push([{
+                phonenumber: "",
+                simcard: "",
+                priceplan: "",
+                search: "",
+            }]);
         }
-    }];
 
+        $scope.removeGroup = function(numIndex) {
+            $scope.detailPhonenumber.splice(numIndex, 1);
+        }
 
-//var customersubtype_val = 'RPI';
-//$scope.customer_subtype.val() = customersubtype_val;
-$scope.customer_subtype = $scope.openservice_obj.customer_subtype[1].textvalue;
-$scope.promotion = $scope.openservice_obj.postPromotion[1].textvalue;
-
-    $scope.addGroup = function() {
-       $scope.detailPhonenumber.push( [{
-            phonenumber: "",
-            simcard: "",
-            priceplan: "",
-            search: "",
-        }]);            
-    }
-
-    $scope.removeGroup = function(numIndex) {
-        $scope.detailPhonenumber.splice(numIndex, 1);
-    }
-
-$scope.gotoNext = function(){
-    console.log();
-}
+        $scope.gotoNext = function() {
+            $location.path('/ordersummary');
+        }
 
 
     });
